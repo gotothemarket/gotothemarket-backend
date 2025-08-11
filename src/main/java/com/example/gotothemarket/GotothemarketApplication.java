49c2e53834dc411a0fe73027f6b0f11d806c1bf4
1jae2jae3jae <drawje01@gmail.com>
2025-08-11 16:10:20 +0900

    Chore : spring 세팅

---
+++ src/main/java/com/example/gotothemarket/GotothemarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.gotothemarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GotothemarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GotothemarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gotothemarket/GotothemarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gotothemarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GotothemarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
