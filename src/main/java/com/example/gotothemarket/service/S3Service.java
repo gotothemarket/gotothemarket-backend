5f79ad9cd67b3d4d912e5e76428af7b28bbb7a27
1jae2jae3jae <drawje01@gmail.com>
2025-08-25 02:47:56 +0900

    fix: S3 권한 설정

--- src/main/java/com/example/gotothemarket/service/S3Service.java
+++ src/main/java/com/example/gotothemarket/service/S3Service.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.ObjectCannedACL;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
 
@@ -35,6 +36,7 @@ public class S3Service {
                     .bucket(bucketName)
                     .key(fileName)
                     .contentType(file.getContentType())
+                    .acl(ObjectCannedACL.PUBLIC_READ)
                     .build();
 
             s3Client.putObject(putObjectRequest,
